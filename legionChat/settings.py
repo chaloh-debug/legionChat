e1c4037d1fa7e8db25605883a291ae35008c25f6
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-10-31 18:02:01 +0300

    add allowed host

--- legionChat/settings.py
+++ legionChat/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['legionchat-33bfa2fdd024.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
