b9925eb9f48af7f538e96b0a106fccd01ad42e4f
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-11-01 11:32:16 +0300

    change db settings

--- legionChat/settings.py
+++ legionChat/settings.py
@@ -102,24 +102,24 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#        "TEST": {
-#            "NAME": BASE_DIR / "db.sqlite3",
-#        },
-#    }
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'legionchat',
-        'USER': 'chaloh',
-        'PASSWORD': 'chaloh123',
-        'HOST': 'localhost',
-        'PORT': '',
-    }}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        "TEST": {
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
+    }
+}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'legionchat',
+#        'USER': 'chaloh',
+#        'PASSWORD': 'chaloh123',
+#        'HOST': 'localhost',
+#        'PORT': '',
+#    }}
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
