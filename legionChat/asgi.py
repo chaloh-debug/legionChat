36df772d4c94ed76ba256effeaf53b47c46c678d
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-11-02 19:11:35 +0300

    settings config

--- legionChat/asgi.py
+++ legionChat/asgi.py
@@ -6,13 +6,17 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from chat.routing import websocket_urlpatterns
+from django.conf import settings
+settings.configure()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "legionChat.settings")
 
 django_asgi_app = get_asgi_application()
 
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
+        "https": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
