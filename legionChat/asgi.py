f7866db7f222705dc5495dbae76225376f60e2f9
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-11-02 19:14:40 +0300

    Procfile config

--- legionChat/asgi.py
+++ legionChat/asgi.py
@@ -1,15 +1,15 @@
 import os
-import chat.routing
+from django.conf import settings
+settings.configure()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "legionChat.settings")
 
+import chat.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from chat.routing import websocket_urlpatterns
-from django.conf import settings
-settings.configure()
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "legionChat.settings")
 
 django_asgi_app = get_asgi_application()
 
